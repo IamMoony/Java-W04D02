18f01f9abe253c96949264472be2b74a4b124bb1
Simon Blaha <s.blaha@live.at>
2019-05-28 10:12:49 +0200

    push basic a1 endresult

--- src/Basic_A1/AccountTest.java
+++ src/Basic_A1/AccountTest.java
@@ -8,27 +8,37 @@ class AccountTest {
 
     @Test
     void getName() {
-        Account myAccount = new Account("Simon The Great", 01020103, 1423);
-        assertEquals("Simon The Great", myAccount.getName());
+        Account myAccount = new Account("Simon", 02233, 1423);
+        assertEquals("Simon", myAccount.getName());
     }
 
     @Test
     void setName() {
+        Account myAccount = new Account("Annina", 04455, 1933);
+        assertEquals("Anna", myAccount.getName());
     }
 
     @Test
     void getAccNumber() {
+        Account myAccount = new Account("Mark", 12234, 1234);
+        assertEquals(12234, myAccount.getAccNumber());
     }
 
     @Test
     void setAccNumber() {
+        Account myAccount = new Account("Benjamin", 12233, 1423);
+        assertEquals(12233, myAccount.getAccNumber());
     }
 
     @Test
     void getAccPin() {
+        Account myAccount = new Account("Warmhud", 14455, 1556);
+        assertEquals(14455, myAccount.getAccPin());
     }
 
     @Test
     void setAccPin() {
+        Account myAccount = new Account("Gregor", 15566, 1342);
+        assertEquals(1342, myAccount.getAccPin());
     }
 }
