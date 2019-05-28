76ec5d609ff45e72ee8c4db1a95d60555d2a73a1
Annina Henninger <annina.henninger@aon.at>
2019-05-28 13:04:49 +0200

    adds some testing methods

--- src/Intermediate_A2/LibraryTest.java
+++ src/Intermediate_A2/LibraryTest.java
@@ -1,28 +1,50 @@
 package Intermediate_A2;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
+    private static final Logger log = LoggerFactory.getLogger(LibraryTest.class);
 
     @BeforeEach
     void init() {
-        log.info("@BeforeEach - executes before each test method in this class");
+        log.info("before each method of Library");
     }
 
     @AfterEach
     void tearDown() {
+        log.info("after each method of Library");
     }
 
     @Test
     void rentBook() {
+        int expected = 4;
+        int actual = 2 + 2;
+        assertEquals(expected, actual, "INCONCEIVABLE!");
+        //
+        Object nullValue = null;
+        assertFalse(nullValue != null);
+        assertNull(nullValue);
+        assertNotNull("A String", "INCONCEIVABLE!");
+        assertTrue(nullValue == null);
     }
 
     @Test
+    @DisplayName("Dummy test")
     void returnBook() {
+        int expected = 4;
+        int actual = 2 + 2;
+        Object nullValue = null;
+
+        assertAll(
+                "Assert All of these",
+                () -> assertEquals(expected, actual, "INCONCEIVABLE!"),
+                () -> assertFalse(nullValue != null),
+                () -> assertNull(nullValue),
+                () -> assertNotNull("A String", "INCONCEIVABLE!"),
+                () -> assertTrue(nullValue == null));
     }
 }
